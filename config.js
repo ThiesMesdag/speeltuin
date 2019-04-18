84aebab959ce6e2b5b70e9feaf6093742f7ed326
mesdat <thies.mesdag@kadaster.nl>
2019-04-18 13:45:08 +0200

    gebaseerd op geonovum respec

---
+++ config.js
@@ -0,0 +1,37 @@
+var respecConfig = {
+  specStatus: "GN-WV",
+  specType: "IM",
+  //canonicalURI: "WV",
+  //publishDate: "2019-03-25",
+  maxTocLevel: 3,
+  editors: [
+    {
+      name: "Kadaster",
+      url: "https://www.kadaster.nl",
+    },
+//    {
+//      name: "Geonovum",
+//      url: "https://www.geonovum.nl", 
+//    },  
+  ],
+  logos: [{
+		src: 'https://zakelijk.kadaster.nl/iv-kadaster-theme/images/logo.png',
+		href: "https://zakelijk.kadaster.nl/imkad",
+		alt: "Kadaster logo",
+		id: 'KadasterLogo',
+  }]
+  shortName: "IMKAD",
+  pubDomain: "IMKAD",
+  github: "https://github.com/Kadaster/imkad",
+  previousPublishDate: "2012-05-08",
+  previousStatus: "GN-CV",
+  //localBiblio: {
+  //  "ISO19136": {
+  //    "href": "https://www.iso.org/standard/32554.html",
+  //    "title": "ISO 19136:2007 Geographic information -- Geography Markup Language (GML)",
+  //    "authors": [""],
+  //    "date": "September 2007",
+  //    "publisher": "International Organization for Standardization",
+  //  },
+  //},
+};
