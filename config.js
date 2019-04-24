b8325ca80b9a156bdfd12f9363d77261a491e6e1
ThiesMesdag <ThiesMesdag@users.noreply.github.com>
2019-04-24 15:57:12 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,25 +1,30 @@
 var respecConfig = {
   specStatus: "GN-WV",
   specType: "IM",
-  //canonicalURI: "WV",
-  //publishDate: "2019-03-25",
-  maxTocLevel: 3,
-  editors: [
-    {
-      name: "Kadaster",
-      url: "https://www.kadaster.nl",
-    },
-  ],
-  logos: [{
+  publishDate: "2019-05-01",
+  editors: [{
+    name: "Thies Mesdag",
+    company: "Kadaster",
+    companyURL: "www.kadadaster.nl",
+  }],
+  shortName: "Kadaster",
+  pubDomain: "IMKAD",
+  github: "https://github.com/kadaster/imkad",
+logos: [{
 		src: 'https://zakelijk.kadaster.nl/iv-kadaster-theme/images/logo.png',
 		href: "https://zakelijk.kadaster.nl/imkad",
 		alt: "Kadaster logo",
 		id: 'KadasterLogo',
-  }]
-  //shortName: "IMKAD",
-  pubDomain: "IMKAD",
-  github: "https://github.com/Kadaster/imkad",
-  previousPublishDate: "2012-05-08",
-  previousStatus: "GN-CV",
-
+  }],
+  //previousPublishDate: "2017-01-01",
+  //previousStatus: "GN-CV",
+  //localBiblio: {
+  //  "ISO19136": {
+  //    "href": "https://www.iso.org/standard/32554.html",
+  //    "title": "ISO 19136:2007 Geographic information -- Geography Markup Language (GML)",
+  //    "authors": [""],
+  //    "date": "September 2007",
+  //    "publisher": "International Organization for Standardization",
+  //  },
+  //},
 };
